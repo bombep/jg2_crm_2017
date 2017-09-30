cac90689ad014d2a85c2e9ad3263ec4422f2c0ed
ascukins <a.scukins@gmail.com>
2017-09-30 14:50:27 +0300

    added java file with comments

---
+++ src/main/java/MainCRM.java
@@ -0,0 +1,34 @@
+package java;
+
+/*
+Things to do:
+create database CRM;
+use CRM;
+create table emp(id int(10),name varchar(40),age int(3));
+create table CRMusers( id int(10), login varchar(50), realname varchar (100), role varchar(50), password varchar(50));
+create table CRMcustomers( id int(10), nickname varchar(50), realname varchar (100),
+company varchar(50),
+profession varchar(50),
+address varchar(50),
+phone varchar(50),
+creationdatetime DATETIME,
+lastupdatedatetime DATETIME
+ );
+..........
+
+
+
+ */
+
+
+
+public class MainCRM {
+
+    static void main (String args[]){
+
+
+    }
+
+
+
+}
