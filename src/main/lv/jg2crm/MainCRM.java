aecd35acdde90074651c2163e14bd89c76588b94
ascukins <32372971+ascukins@users.noreply.github.com>
2017-10-24 16:33:25 +0300

    Update MainCRM.java
    moved DB creation script to a separate file

--- src/main/lv/jg2crm/MainCRM.java
+++ src/main/lv/jg2crm/MainCRM.java
@@ -1,46 +1,17 @@
 package lv.jg2crm;
 
-/*
-Things to do:
-create database CRM;
-use CRM;
-create table CRMusers(
-    id int NOT NULL AUTO_INCREMENT,
-    username varchar(50), realname varchar (100), role varchar(50), password varchar(50), PRIMARY KEY (id)
-);
+/* database creation script was moved to a separate file
 
-create table CRMcustomers(
-    id int NOT NULL AUTO_INCREMENT,
-    nickname varchar(50), realname varchar (100),
+    Customers:
+    id int,
+    nickname varchar(50), 
+    realname varchar (100),
     company varchar(50),
     profession varchar(50),
     address varchar(50),
     phone varchar(50),
     creationdatetime DATETIME,
     lastupdatedatetime DATETIME,
-    PRIMARY KEY (id)
-);
-
-create table CRMcustomfields(
-    id int NOT NULL AUTO_INCREMENT,
-    custid int NOT NULL,
-    fieldname varchar(50), fieldvalue varchar (200),
-    PRIMARY KEY (id),
-    FOREIGN KEY (custid)   REFERENCES CRMcustomers(id)
-);
-
-create table CRMevents(
-    id int NOT NULL AUTO_INCREMENT,
-    custid int NOT NULL,
-    userid int NOT NULL,
-    startdatetime DATETIME,
-    enddatetime DATETIME,
-    eventtype varchar(50), text varchar (500),
-    PRIMARY KEY (id),
-    FOREIGN KEY (custid)   REFERENCES CRMcustomers(id),
-    FOREIGN KEY (userid)   REFERENCES CRMusers(id)
-);
-
 
 Logic behind the events table
 ------------------------------------------------
